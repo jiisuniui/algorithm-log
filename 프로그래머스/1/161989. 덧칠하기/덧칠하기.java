b31bfad9376e747a96992f01d7d2350b9a4e13a9
jiisuniui <82515938+jiisuniui@users.noreply.github.com>
2023-10-11 12:00:29 +0900

    [level 1] Title: 덧칠하기, Time: 0.98 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        boolean[] paint = new boolean[section.length];
+        
+        int painted = 0;
+        for(int i=0; i<section.length; i++){
+            if(!paint[i]){
+                if(painted<section[i]){
+                    answer++;
+                    painted=section[i]+m-1;
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}
