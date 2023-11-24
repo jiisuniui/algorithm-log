cf696629f6e4b4775df6328477683b1d9a8a96e4
jiisuniui <82515938+jiisuniui@users.noreply.github.com>
2023-11-24 12:32:00 +0900

    [level 1] Title: 문자열 나누기, Time: 1.51 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,42 +1,3 @@
-// import java.util.*;
-
-// class Solution {
-//     public int solution(String s) {
-//         int answer = 0;
-//         List<String> list = new ArrayList<>();
-        
-//         while(!s.equals("")){
-//             char first = s.charAt(0);
-//             int fnum = 1;
-//             int onum = 0;
-//             int i=1;
-//             for(i=1; i<s.length(); i++){
-//                 if(first == s.charAt(i)){
-//                     fnum++;
-//                 } else {
-//                     onum++;
-//                 }
-//                 if(fnum==onum){ 
-//                     list.add(s.substring(0,i+1));
-//                     s=s.substring(i+1,s.length());
-//                     break;
-//                 }
-//             }
-            
-//             if(i==s.length()){
-//                 if(s.equals("")){
-//                     break;
-//                 }
-//                 list.add(s);
-//                 s="";
-//                 break;
-//             }
-//         }
-        
-//         return list.size();
-//     }
-// }
-
 import java.util.*;
 
 class Solution {
@@ -44,7 +5,7 @@ class Solution {
         List<String> list = new ArrayList<>();
         
         int fnum = 0, onum = 0; 
-        int startIdx = 0; // start index of the current substring
+        int startIdx = 0;
 
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i) == s.charAt(startIdx)){
@@ -55,13 +16,12 @@ class Solution {
             
             if(fnum == onum){
                 list.add(s.substring(startIdx, i+1));
-                startIdx = i+1; // set start index for the next substring
-                fnum = 0; // reset counters
+                startIdx = i+1;
+                fnum = 0;
                 onum = 0;
             }
         }
 
-        // If there's any remaining string after the loop, add it to the list
         if(startIdx < s.length()){
             list.add(s.substring(startIdx));
         }
