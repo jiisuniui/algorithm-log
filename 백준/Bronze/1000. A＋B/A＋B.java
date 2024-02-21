93d60ca9a8c0cbf7edd1b2e133973be2f787ae0a
jiisuniui <82515938+jiisuniui@users.noreply.github.com>
2024-02-21 23:53:11 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scann = new Scanner(System.in);
+        int a = scann.nextInt();
+        int b = scann.nextInt();
+        System.out.println(a+b);
+    }
+}
