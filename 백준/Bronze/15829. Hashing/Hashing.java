04c447f2372d30507002f5a882292c5a0dd7ff87
jiisuniui <82515938+jiisuniui@users.noreply.github.com>
2024-12-13 19:06:00 +0900

    [Bronze II] Title: Hashing, Time: 176 ms, Memory: 17724 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scann = new Scanner(System.in);
-        int L = scann.nextInt();
+        long M = 1234567891;
+        long L = scann.nextInt();
         String str = scann.next();
         long hash = 0;
         for (int i = 0; i < L; i++) {
-            int alpha = str.charAt(i) - 'a';
-            hash += (alpha + 1) * Math.pow(31, i);
+            long alpha = str.charAt(i) - 'a' + 1;
+            for (int j = 0; j < i; j++) {
+                alpha *= 31;
+                alpha %= M;
+            }
+            hash = (hash + alpha) % M;
         }
         System.out.println(hash);
     }
