f8623ccafe4097349c7120524cb2ce705e539247
jiisuniui <82515938+jiisuniui@users.noreply.github.com>
2024-12-24 14:18:14 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2628 ms, Memory: 327720 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -5,41 +5,24 @@ public class Main {
         Scanner scann = new Scanner(System.in);
         int N = scann.nextInt();
         int M = scann.nextInt();
-        int[][] arr = new int[N][N];
-        int[][] sum = new int[N][N];
+        int[][] arr = new int[N + 1][N + 1];
+        int[][] sum = new int[N + 1][N + 1];
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
                 arr[i][j] = scann.nextInt();
-
-                if (i == 0 && j == 0) {
-                    sum[i][j] = arr[i][j];
-                } else if (i == 0) {
-                    sum[i][j] = arr[i][j] + sum[i][j - 1];
-                } else if (j == 0) {
-                    sum[i][j] = arr[i][j] + sum[i - 1][j];
-                } else {
-                    sum[i][j] = arr[i][j] + sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1];
-                }
+                sum[i][j] = arr[i][j] + sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1];
             }
         }
 
         for (int c = 0; c < M; c++) {
-            int x1 = scann.nextInt() - 1;
-            int y1 = scann.nextInt() - 1;
-            int x2 = scann.nextInt() - 1;
-            int y2 = scann.nextInt() - 1;
+            int x1 = scann.nextInt();
+            int y1 = scann.nextInt();
+            int x2 = scann.nextInt();
+            int y2 = scann.nextInt();
 
             int res = 0;
-            if (x1 == 0 && y1 == 0) {
-                res = sum[x2][y2];
-            } else if (x1 == 0) {
-                res = sum[x2][y2] - sum[x2][y1 - 1];
-            } else if (y1 == 0) {
-                res = sum[x2][y2] - sum[x1 - 1][y2];
-            } else {
-                res = sum[x2][y2] - sum[x2][y1 - 1] - sum[x1 - 1][y2] + sum[x1 - 1][y1 - 1];
-            }
+            res = sum[x2][y2] - sum[x2][y1 - 1] - sum[x1 - 1][y2] + sum[x1 - 1][y1 - 1];
 
             System.out.println(res);
         }
