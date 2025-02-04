13a9f662dbcb26a2386ca7aa0f7228b565193aff
jiisuniui <82515938+jiisuniui@users.noreply.github.com>
2025-02-04 11:38:49 +0900

    [Silver I] Title: 기타 레슨, Time: 784 ms, Memory: 108004 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int[] video;
+    static int N, M;
+
+    static int start, end = 0;
+
+    public static void main(String[] args) {
+        Scanner scann = new Scanner(System.in);
+        N = scann.nextInt();
+        M = scann.nextInt();
+        video = new int[N];
+        for (int i = 0; i < N; i++) {
+            video[i] = scann.nextInt();
+
+            if (start < video[i]) {
+                start = video[i];
+            }
+            end += video[i];
+        }
+        scann.close();
+
+        while (start <= end) {
+            int middle = (start + end) / 2;
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                if (sum + video[i] > middle) {
+                    count++;
+                    sum = 0;
+                }
+                sum = sum + video[i];
+            }
+
+            if (sum != 0) {
+                count++;
+            }
+
+            if (count > M) {
+                start = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+
+        System.out.println(start);
+    }
+
+}
